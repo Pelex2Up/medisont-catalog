112c919c53d45cade065559fc9405e56745111a3
aleksejpetrovec <rbtechologics@mail.ru>
2025-05-26 11:27:12 +0300

    header upd;

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,13 @@ import styles from "./header.module.scss";
 import logo from "./assets/image.png";
 import { about, printing, realise } from "./config";
 import Hamburger from "hamburger-react";
-import { Box, Modal, Typography } from "@mui/material";
+import { Box, Modal, Typography, useMediaQuery } from "@mui/material";
 import phoneImg from "./assets/modal.png";
+import miniLogo from "./assets/mini logo.png";
+
+interface IHeader {
+  smallHeader: boolean;
+}
 
 const style = {
   position: "absolute",
@@ -17,21 +22,42 @@ const style = {
   boxShadow: 24,
 };
 
-export const Header: FC = () => {
+export const Header: FC<IHeader> = ({ smallHeader }) => {
   const [open, setOpen] = useState(false);
   const [openModal, setOpenModal] = useState(false);
+  const media = useMediaQuery(`(min-width:1300px)`);
+
   return (
-    <section className={styles.wrapper}>
-      <a href="https://medisont.by/">
-        <img className={styles.wrapper_logo} src={logo} alt="logo" />
+    <section
+      className={`${smallHeader && styles.miniHeader} ${
+        !media && styles.header
+      } ${styles.wrapper}`}
+      id="header-section"
+    >
+      <a
+        style={smallHeader ? { height: "35px" } : {}}
+        href="https://medisont.by/"
+      >
+        <img
+          className={`${smallHeader && styles.miniHeader_logo} ${
+            styles.wrapper_logo
+          }`}
+          src={smallHeader ? miniLogo : logo}
+          alt="logo"
+        />
       </a>
 
       <nav className={styles["wrapper_navBlock"]}>
         <span
           // href="https://medisont.by/#submenu:catalog"
-          className={styles["wrapper_navBlock_navElement"]}
+          className={`${smallHeader && styles.miniHeader_link} ${
+            styles["wrapper_navBlock_navElement"]
+          }`}
         >
-          Печатаем <div className={styles.arrow} />
+          Печатаем{" "}
+          <div
+            className={`${smallHeader && styles.smallArrow} ${styles.arrow}`}
+          />
           <ul
             role="menu"
             className={styles.wrapper_navBlock_navElement_dropDownMenu}
@@ -60,9 +86,14 @@ export const Header: FC = () => {
         </span>
         <span
           // href="https://medisont.by/#submenu:izdatelstvo"
-          className={styles["wrapper_navBlock_navElement"]}
+          className={`${smallHeader && styles.miniHeader_link} ${
+            styles["wrapper_navBlock_navElement"]
+          }`}
         >
-          Издаём <div className={styles.arrow} />
+          Издаём{" "}
+          <div
+            className={`${smallHeader && styles.smallArrow} ${styles.arrow}`}
+          />
           <ul
             role="menu"
             className={styles.wrapper_navBlock_navElement_dropDownMenu}
@@ -91,21 +122,30 @@ export const Header: FC = () => {
         </span>
         <a
           href="https://medisont.by/suvenirnaja"
-          className={styles["wrapper_navBlock_navElement"]}
+          className={`${smallHeader && styles.miniHeader_link} ${
+            styles["wrapper_navBlock_navElement"]
+          }`}
         >
           Брендируем
         </a>
         <a
           href="https://medisont.by/price"
-          className={styles["wrapper_navBlock_navElement"]}
+          className={`${smallHeader && styles.miniHeader_link} ${
+            styles["wrapper_navBlock_navElement"]
+          }`}
         >
           Цены
         </a>
         <span
           // href="https://medisont.by/#submenu:onas"
-          className={styles["wrapper_navBlock_navElement"]}
+          className={`${smallHeader && styles.miniHeader_link} ${
+            styles["wrapper_navBlock_navElement"]
+          }`}
         >
-          О нас <div className={styles.arrow} />
+          О нас{" "}
+          <div
+            className={`${smallHeader && styles.smallArrow} ${styles.arrow}`}
+          />
           <ul
             role="menu"
             className={styles.wrapper_navBlock_navElement_dropDownMenu}
@@ -134,88 +174,100 @@ export const Header: FC = () => {
         </span>
         <a
           href="https://medisont.by/kontakty"
-          className={styles["wrapper_navBlock_navElement"]}
+          className={`${smallHeader && styles.miniHeader_link} ${
+            styles["wrapper_navBlock_navElement"]
+          }`}
           style={{ padding: 0 }}
         >
           Контакты
         </a>
       </nav>
-      <div
-        className={styles["wrapper_phone"]}
-        style={{
-          lineHeight: "18px",
-          minWidth: "156px",
-          display: "flex",
-          flexDirection: "column",
-          gap: "1px",
-          marginTop: "-10px",
-        }}
-      >
+      {!smallHeader && (
+        <div
+          className={styles["wrapper_phone"]}
+          style={{
+            lineHeight: "18px",
+            minWidth: "156px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "1px",
+            marginTop: "-10px",
+          }}
+        >
+          <a
+            href="tel:+375173000093"
+            style={{
+              fontSize: "32px",
+              width: "100%",
+              textAlign: "justify",
+              lineHeight: 1,
+              color: "#000000",
+              textDecoration: "none",
+            }}
+          >
+            <strong
+              style={{ fontFamily: `"Segoe UI Bold", Arial, sans-serif` }}
+            >
+              300-00-93
+            </strong>
+          </a>
+          <span
+            style={{
+              fontSize: "14px",
+              width: "100%",
+              textAlign: "justify",
+              textDecoration: "none",
+              color: "#000000",
+            }}
+          >
+            многоканальный город
+          </span>
+          <a
+            href="tel:+375333970093"
+            style={{
+              fontSize: "14px",
+              width: "100%",
+              textAlign: "justify",
+              color: "#000000",
+              textDecoration: "none",
+            }}
+          >
+            +375{" "}
+            <strong
+              style={{ fontFamily: `"Segoe UI Bold", Arial, sans-serif` }}
+            >
+              33 397-00-93
+            </strong>{" "}
+            моб
+          </a>
+          <a
+            href="tel:+375292007410"
+            style={{
+              fontSize: "14px",
+              width: "100%",
+              textAlign: "justify",
+              color: "#000000",
+              textDecoration: "none",
+            }}
+          >
+            +375{" "}
+            <strong
+              style={{ fontFamily: `"Segoe UI Bold", Arial, sans-serif` }}
+            >
+              29 200-74-10
+            </strong>{" "}
+            моб
+          </a>
+        </div>
+      )}
+      {!smallHeader && (
         <a
-          href="tel:+375173000093"
-          style={{
-            fontSize: "32px",
-            width: "100%",
-            textAlign: "justify",
-            lineHeight: 1,
-            color: "#000000",
-            textDecoration: "none",
-          }}
+          href="https://drive.google.com/file/d/1M8ttAtyCn8qHtX5s6gBf7pEMue4wWa3l/view?usp=sharing"
+          className={styles.button}
         >
-          <strong style={{ fontFamily: `"Segoe UI Bold", Arial, sans-serif` }}>
-            300-00-93
-          </strong>
+          PDF-каталог
         </a>
-        <span
-          style={{
-            fontSize: "14px",
-            width: "100%",
-            textAlign: "justify",
-            textDecoration: "none",
-            color: "#000000",
-          }}
-        >
-          многоканальный город
-        </span>
-        <a
-          href="tel:+375333970093"
-          style={{
-            fontSize: "14px",
-            width: "100%",
-            textAlign: "justify",
-            color: "#000000",
-            textDecoration: "none",
-          }}
-        >
-          +375{" "}
-          <strong style={{ fontFamily: `"Segoe UI Bold", Arial, sans-serif` }}>
-            33 397-00-93
-          </strong>{" "}
-          моб
-        </a>
-        <a
-          href="tel:+375292007410"
-          style={{
-            fontSize: "14px",
-            width: "100%",
-            textAlign: "justify",
-            color: "#000000",
-            textDecoration: "none",
-          }}
-        >
-          +375{" "}
-          <strong style={{ fontFamily: `"Segoe UI Bold", Arial, sans-serif` }}>
-            29 200-74-10
-          </strong>{" "}
-          моб
-        </a>
-      </div>
-      <a
-        href="https://drive.google.com/file/d/1M8ttAtyCn8qHtX5s6gBf7pEMue4wWa3l/view?usp=sharing"
-        className={styles.button}
-      >
-        PDF-каталог
-      </a>
+      )}
       <div className={styles["wrapper_mobileHeader"]}>
         <div className={styles["wrapper_mobileHeader_text"]}>
           <span className={styles["wrapper_mobileHeader_text_top"]}>
@@ -354,7 +406,12 @@ export const Header: FC = () => {
               Контакты
             </a>
           </div>
-          <button onClick={() => setOpenModal(true)} className={styles.phoneButton}>Позвонить</button>
+          <button
+            onClick={() => setOpenModal(true)}
+            className={styles.phoneButton}
+          >
+            Позвонить
+          </button>
           <Modal
             open={openModal}
             onClose={() => setOpenModal(false)}
@@ -373,9 +430,9 @@ export const Header: FC = () => {
                   fontSize: "24px",
                   fontWeight: "600",
                   textAlign: "center",
-                  padding: '1rem 1rem 0',
-                  lineHeight: '26px',
-                  fontFamily: "'Segoe UI Semibold', sans-serif"
+                  padding: "1rem 1rem 0",
+                  lineHeight: "26px",
+                  fontFamily: "'Segoe UI Semibold', sans-serif",
                 }}
               >
                 Нажмите на номер для вызова
--- src/components/mainLayout/index.tsx
+++ src/components/mainLayout/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Outlet } from "react-router";
 import styles from "./mainLayout.module.scss";
 import { Header } from "../Header";
@@ -6,12 +6,36 @@ import { UpperFooterBlock } from "../upperFooterBlock";
 import { Footer } from "../Footer";
 import { Cart } from "../Cart";
 import { useCart } from "react-use-cart";
+import { useMediaQuery } from "@mui/material";
 
 export const MainLayout: FC = () => {
   const { items } = useCart();
+  const [headerSmall, setHeaderSmall] = useState<boolean>(false);
+  const media = useMediaQuery(`(min-width:1300px)`);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const blockHeight =
+        document.getElementById("header-section")?.offsetHeight ?? 0;
+
+      if (media && scrollPosition > blockHeight - 50) {
+        setHeaderSmall(true);
+      } else {
+        setHeaderSmall(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [headerSmall, setHeaderSmall]);
+
   return (
     <div className={styles.wrapper}>
-      <Header />
+      <Header smallHeader={headerSmall} />
       {items.length > 0 && <Cart />}
       <div className={styles.wrapper_body}>
         <Outlet />
